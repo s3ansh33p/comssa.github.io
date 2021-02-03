983192e6f600c5f0cb8aeb4365b90a94af885bd0
Kevin Le <le.kevin838@gmail.com>
2021-02-03 14:20:48 +0800

    Navbar

---
+++ comssa/src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="app">
+    <NavBar/>
+  </div>
+</template>
+
+<script>
+import NavBar from './components/NavBar.vue'
+
+export default {
+  name: 'App',
+  components: {
+    NavBar
+  }
+}
+</script>
+
+<style>
+  body{
+      margin:0px;
+  } 
+
+  * {
+    font-family: 'Montserrat', sans-serif;
+  }
+</style>
---
+++ comssa/src/components/NavBar.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="navbar">
+        <img id="logo" src="../assets/img/comssa.jpg">
+
+        <div class="links" id="links">
+            <ul>
+                <li><a href="#">About</a></li>
+                <li><a href="#">Events</a></li>
+                <li><a href="#">Sponsors</a></li>
+                <li><button>Join Now</button></li>
+            </ul> 
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'NavBar',
+}
+</script>
+
+<style>
+@import '../assets/css/navbar.css';
+</style>
