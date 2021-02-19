0fb5e1b48a9fbcf6a4548fdf40b5477eaa0b262f
Anurag <anuragsingh3@outlook.com>
2021-02-19 12:31:38 +0800

    remove default bootstrap navbar colour

--- components/NavBar.vue
+++ components/NavBar.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="navbar">
-    <b-navbar :class="scrolled ? 'shadow' : ''" toggleable="md" type="light" variant="light">
+    <b-navbar :class="scrolled ? 'shadow' : ''" toggleable="md">
       <b-navbar-brand id="logo" to="/">
         <img id="logo" src="~/assets/img/branding/comssa.png">
       </b-navbar-brand>
