abed017756045d02d8897a21761ca30b0a83dcb9
Anurag <anuragsingh3@outlook.com>
2021-03-02 17:51:40 +0800

    Catch all linking, 404 on anything not within the redirects obj

---
+++ pages/_.vue
@@ -0,0 +1,15 @@
+<script>
+const redirects = {
+    grillthedepartment: "https://forms.gle/dJqPdR6KaBC3aNQd8"
+}
+
+export default {
+    asyncData ({ redirect, error, params }) {
+        if (!(params.pathMatch in redirects)) {
+            return error({ statusCode: 404 })
+        }
+
+        return redirect(301, redirects[params.pathMatch])
+    }
+}
+</script>
