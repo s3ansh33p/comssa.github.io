9b992eb5d8e05ceb737f274f431f6ce758a092ef
Anurag <anuragsingh3@outlook.com>
2021-03-04 15:32:14 +0800

    Handle minutes

---
+++ pages/minutes/_minute.vue
@@ -0,0 +1,21 @@
+<template>
+  <b-container fluid>
+    <h1>{{ page.title }}</h1>
+    <nuxt-content :document="page" />
+  </b-container>
+</template>
+
+<script>
+export default {
+    async asyncData ({ $content, params }) {
+        const page = await $content("minutes", params.minute, "index").fetch()
+
+        return {
+            page
+        }
+    },
+    head: {
+        title: "Minutes"
+    }
+}
+</script>
