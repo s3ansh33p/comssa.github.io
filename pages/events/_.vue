d59a5612304d4e9c9314c290ca1c1db9db4e9d62
Anurag <anuragsingh3@outlook.com>
2021-04-23 21:51:11 +0800

    Add sundowner event link redirect

--- pages/events/_.vue
+++ pages/events/_.vue
@@ -1,7 +1,8 @@
 <script>
 const redirects = {
     feedback: "https://forms.gle/R5o4wq9Q2djaLGSx6",
-    suggestions: "https://forms.gle/pEA8MzaRAnABjN5f6"
+    suggestions: "https://forms.gle/pEA8MzaRAnABjN5f6",
+    sundowner: "https://comssasundowner-event.getqpay.com/"
 }
 
 export default {
