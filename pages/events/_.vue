64f7f591f849a4227234815c390a6d8b5875bb22
Anurag Singh <contact@anurag.sh>
2021-08-29 20:34:53 +0800

    Slightly better naming

--- pages/events/_.vue
+++ pages/events/_.vue
@@ -2,7 +2,7 @@
 const redirects = {
     feedback: "https://forms.gle/R5o4wq9Q2djaLGSx6",
     suggestions: "https://forms.gle/pEA8MzaRAnABjN5f6",
-    ComSSA_LAN_Night_2021_S2_1: "https://comssalan.getqpay.com/"
+    "ComSSA-LAN": "https://comssalan.getqpay.com/"
 }
 
 export default {
