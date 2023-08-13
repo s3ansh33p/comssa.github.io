d2679bf09caf1eb38e0934886c7f86a14d55be66
justinp04 <justin.pan688@gmail.com>
2023-08-13 23:31:30 +0800

    Added spacing to the sponsors section
    Felt that it was too cluttered and hard to distinguish the tiers. I added some margins and padding to space out the tiers to give each one its own distinct section.

--- pages/sponsors.vue
+++ pages/sponsors.vue
@@ -10,86 +10,92 @@
       For any inquiries about sponsorship, please reach out to <a href="mailto:sponsorship@comssa.org.au">sponsorship@comssa.org.au</a>.
     </p>
 
-    <h2 class="text-center">Gold Tier</h2>
-    <b-row align-h="around" class="mb-5 align-items-center">
-      <b-col cols="4">
-        <a href="https://www.vgw.co/">
-          <b-img
-            width="450"
-            :src="'https://res.cloudinary.com/dec6mijn3/image/upload/v1686654371/ComSSA-Website/Sponsors/vgw.png'"
-            fluid
-          />
-        </a>
-      </b-col>
+    <div class="mt-5 pb-1">
+      <h2 class="text-center mb-2">Gold Tier</h2>
+      <b-row align-h="around" class="mb-5 align-items-center">
+        <b-col cols="4">
+          <a href="https://www.vgw.co/">
+            <b-img
+              width="450"
+              :src="'https://res.cloudinary.com/dec6mijn3/image/upload/v1686654371/ComSSA-Website/Sponsors/vgw.png'"
+              fluid
+            />
+          </a>
+        </b-col>
 
-      <b-col cols="4" class="tomahawkLabs">
-        <a href="https://tomahawklabs.com.au/">
-          <b-img
-            width="450"
-            :src="'https://res.cloudinary.com/dec6mijn3/image/upload/v1679834808/ComSSA-Website/Sponsors/tomahawk-labs.png'"
-            fluid
-          />
-        </a>
-      </b-col>
-    </b-row>
+        <b-col cols="4" class="tomahawkLabs">
+          <a href="https://tomahawklabs.com.au/">
+            <b-img
+              width="450"
+              :src="'https://res.cloudinary.com/dec6mijn3/image/upload/v1679834808/ComSSA-Website/Sponsors/tomahawk-labs.png'"
+              fluid
+            />
+          </a>
+        </b-col>
+      </b-row>
+    </div>
 
-    <h2 class="text-center">Silver Tier</h2>
-    <b-row align-h="around" class="mb-5 align-items-center">
-      <b-col cols="4">
-        <a href="https://www.atlassian.com/">
-          <b-img
-            width="450"
-            :src="'https://res.cloudinary.com/dec6mijn3/image/upload/v1679834808/ComSSA-Website/Sponsors/atlassian.png'"
-            fluid
-          />
-        </a>
-      </b-col>
-    </b-row>
+    <div class="mt-5 pb-1">
+      <h2 class="text-center mb-2">Silver Tier</h2>
+      <b-row align-h="around" class="mb-5 align-items-center">
+        <b-col cols="4">
+          <a href="https://www.atlassian.com/">
+            <b-img
+              width="450"
+              :src="'https://res.cloudinary.com/dec6mijn3/image/upload/v1679834808/ComSSA-Website/Sponsors/atlassian.png'"
+              fluid
+            />
+          </a>
+        </b-col>
+      </b-row>
+    </div>
 
-    <h2 class="text-center">Bronze Tier</h2>
-    <b-row align-h="around" class="mb-3 align-items-center">
-      <b-col cols="4">
-        <a href="https://www.imc.com/">
-          <b-img
-            width="450"
-            :src="'https://res.cloudinary.com/dec6mijn3/image/upload/v1679834808/ComSSA-Website/Sponsors/imc.png'"
-            fluid
-          />
-        </a>
-      </b-col>
+    <div class="mt-5 pb-1">
+      <h2 class="text-center mb-2">Bronze Tier</h2>
+      <b-row align-h="around" class="mb-3 align-items-center">
+        <b-col cols="4">
+          <a href="https://www.imc.com/">
+            <b-img
+              width="450"
+              :src="'https://res.cloudinary.com/dec6mijn3/image/upload/v1679834808/ComSSA-Website/Sponsors/imc.png'"
+              fluid
+            />
+          </a>
+        </b-col>
 
-      <b-col cols="4">
-        <a href="https://www.bankwest.com.au/">
-          <b-img
-            width="450"
-            :src="'https://res.cloudinary.com/dec6mijn3/image/upload/v1679834808/ComSSA-Website/Sponsors/bankwest.png'"
-            fluid
-          />
-        </a>
-      </b-col>
-    </b-row>
+        <b-col cols="4">
+          <a href="https://www.bankwest.com.au/">
+            <b-img
+              width="450"
+              :src="'https://res.cloudinary.com/dec6mijn3/image/upload/v1679834808/ComSSA-Website/Sponsors/bankwest.png'"
+              fluid
+            />
+          </a>
+        </b-col>
+      </b-row>
 
-    <b-row align-h="around" class="mb-5 align-items-center">
-      <b-col cols="4">
-        <a href="https://redisoftware.com.au/">
-          <b-img
-            width="450"
-            :src="'https://res.cloudinary.com/dec6mijn3/image/upload/v1679834808/ComSSA-Website/Sponsors/redi-software.png'"
-            fluid
-          />
-        </a>
-      </b-col>
+      <b-row align-h="around" class="mb-5 align-items-center">
+        <b-col cols="4">
+          <a href="https://redisoftware.com.au/">
+            <b-img
+              width="450"
+              :src="'https://res.cloudinary.com/dec6mijn3/image/upload/v1679834808/ComSSA-Website/Sponsors/redi-software.png'"
+              fluid
+            />
+          </a>
+        </b-col>
 
-      <b-col cols="4">
-        <a href="https://www.mechanicalrock.io/">
-          <b-img
-            width="450"
-            :src="'https://res.cloudinary.com/dec6mijn3/image/upload/c_scale,w_450/v1691926726/ComSSA-Website/Sponsors/mechanical-rock.webp'"
-            fluid
-          />
-        </a>
-      </b-col>
-    </b-row>
+        <b-col cols="4">
+          <a href="https://www.mechanicalrock.io/">
+            <b-img
+              width="450"
+              :src="'https://res.cloudinary.com/dec6mijn3/image/upload/c_scale,w_450/v1691926726/ComSSA-Website/Sponsors/mechanical-rock.webp'"
+              fluid
+            />
+          </a>
+        </b-col>
+      </b-row>
+    </div>
 
     <p class="mt-5">
       ComSSA is proudly partnered with the Curtin School of Electrical Engineering, Computing and Mathematical Sciences.
